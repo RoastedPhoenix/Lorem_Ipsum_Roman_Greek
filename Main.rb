fbe8ecbe089d9fdb4f2e2ba65b204f548e7f04bf
RoastedPhoenix <jg51945@students,westport.k12.ct.us>
2015-04-09 21:12:58 -0400

    Lorem Ipsum

--- Main.rb
+++ Main.rb
@@ -2,13 +2,46 @@ require 'bundler'
 Bundler.require
 
 
-get "/ipsum/:paragraphs" do
-  @times = params[:paragraphs].to_i*75
-  @title_ipsum = ["Romam Greeksum", "Greekum Romansum"]
-  @roman_ipsum = ["Saturn", "Iuppiter", "Neptune", "Bacchus", "Volcanius", "Venus", "Pluto", "Mars", "Diana", "Vestia", "Iuno", "Ceres", "Mercury", "Sol"]
-  @greek_ipsum = ["Chronos", "Zeus", "Poseidon", "Dionysus", "Hephaestus", "Aphrodite", "Hades", "Ares", "Artemis", "Hestia", "Hera", "Demeter", "Hermes", "Apollo"]
-  erb :ERB
+get "/" do
+  erb :Generator
 end
 
-
+post '/Ipsum/Generate' do
+  @roman_ipsum = ["Saturn", "Iuppiter", "Neptune", "Bacchus", "Volcanius", "Venus", "Pluto", "Mars", "Diana", "Vestia", "Iuno", "Ceres", "Mercury", "Sol"]
+  @greek_ipsum = ["Chronos", "Zeus", "Poseidon", "Dionysus", "Hephaestus", "Aphrodite", "Hades", "Ares", "Artemis", "Hestia", "Hera", "Demeter", "Hermes", "Apollo"]
+  @title_ipsum = ["Romam Greeksum", "Greekum Romansum"]
+  @romansum = []
+  @greeksum = []
+  if params[:amount] == "pages"
+    params[:quantity].to_i.times do
+      500.times do
+        @r = @roman_ipsum.sample
+        @g = @greek_ipsum.sample
+        @romansum.push @r
+        @greeksum.push @g
+      end
+    end
+  elsif params[:amount] == "paragraphs"
+    params[:quantity].to_i.times do
+      75.times do
+        @r = @roman_ipsum.sample
+        @g = @greek_ipsum.sample
+        @romansum.push @r
+        @greeksum.push @g
+      end
+    end
+  elsif params[:amount] == "words"
+    params[:quantity].to_i.times do
+      1.times do
+        @r = @roman_ipsum.sample
+        @g = @greek_ipsum.sample
+        @romansum.push @r
+        @greeksum.push @g
+      end
+    end
+  end
+  @romansum = @romansum.join(', ')
+  @greeksum = @greeksum.join(', ')
+  erb :Ipsum
+end
 
